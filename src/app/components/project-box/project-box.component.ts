873b600f5d4e412de8d6b2e60f16e0961084931f
Alexandre Gouveia Rodrigues <alexandre@MacBook-Pro.local>
2026-05-07 00:38:05 -0400

    UI changes 3

--- src/app/components/project-box/project-box.component.ts
+++ src/app/components/project-box/project-box.component.ts
@@ -8,4 +8,23 @@ import { ProjectBox } from '../../_models/ProjectBox';
 })
 export class ProjectBoxComponent {
   @Input() projectbox = {} as ProjectBox;
+  mouseX = 0;
+  mouseY = 0;
+  isPointerInside = false;
+
+  onPointerMove(event: MouseEvent): void {
+    const target = event.currentTarget as HTMLElement;
+    const rect = target.getBoundingClientRect();
+    this.mouseX = event.clientX - rect.left;
+    this.mouseY = event.clientY - rect.top;
+  }
+
+  onPointerEnter(event: MouseEvent): void {
+    this.isPointerInside = true;
+    this.onPointerMove(event);
+  }
+
+  onPointerLeave(): void {
+    this.isPointerInside = false;
+  }
 }
