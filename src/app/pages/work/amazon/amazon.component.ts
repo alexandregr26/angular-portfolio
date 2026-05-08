db4050db4257be789e6a92d8770bc0e3d1e961ac
Alexandre Gouveia Rodrigues <alexandre@MacBook-Pro.local>
2026-05-08 15:44:35 -0400

    Amazon 2026 page revamp

--- src/app/pages/work/amazon/amazon.component.ts
+++ src/app/pages/work/amazon/amazon.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Skill } from 'src/app/_models/Skill';
 import { WorkHeader } from 'src/app/_models/WorkHeader';
 import { projects } from 'src/app/data/projects';
 
@@ -12,6 +13,15 @@ export class AmazonComponent {
   header: WorkHeader = {
     img: "assets/img/experience/Amazon/blue.png"
   };
+
+  technologies: Skill[] = [
+    { img: "assets/img/skills/hard/css.png", title: "CSS", level: "Advanced" },
+    { img: "assets/img/skills/hard/git.png", title: "Git", level: "" },
+    { img: "assets/img/skills/hard/html.png", title: "HTML", level: "Advanced" },
+    { img: "assets/img/skills/hard/javascript.png", title: "JavaScript", level: "Advanced" },
+    { img: "assets/img/skills/hard/react.png", title: "React", level: "Intermediate" },
+    { img: "assets/img/skills/hard/redux.png", title: "Redux", level: "Intermediate" }
+  ];
 
   projects = projects;
 
